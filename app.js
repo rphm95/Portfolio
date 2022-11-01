f08136af2ef09e6ba18da96ec3882f99bc118317
rafaela bootcamp <rafaelabootcamp@rafaelas-air.myfiosgateway.com>
2022-11-01 19:57:42 -0400

    home and projects page, not styled yet

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 $ (() => {
-   
+//    ================================== INDEX.HTML MAIN PAGE =====================================
+    
+
     $(".cross").hide();
     $(".menu").hide();
     
@@ -16,8 +18,40 @@ $ (() => {
     $(".hamburger").show();
     });
     });
-        
+
+    // $('.contact').on('click', () => {
+    //     $(".menu").slideToggle("slow", function() {
+    //         $()
+    // }) // work on a contact me button with a modal
+
+// ======================================= PORTFOLIO.HTMLPAGE =======================================
+    
+    let numOfImages = $('.carousel-images').children().length - 1
+    let currentImageIndex = 0
+
+    $('.next-button').on('click', () => {
+        $('.carousel-images').children().eq(currentImageIndex).css('display', 'none')
+
+        if (currentImageIndex < numOfImages) {
+            currentImageIndex++
+        } else { 
+            currentImageIndex = 0
+        }
+
+        $('.carousel-images').children().eq(currentImageIndex).css('display', 'block')
+    })
      
+    $('.previous-button').on('click', () => {
+        $('.carousel-images').children().eq(currentImageIndex).css('display', 'none')
+        if (currentImageIndex > 0) {
+            currentImageIndex--
+        } else { 
+            currentImageIndex = numOfImages
+        }
+        $('.carousel-images').children().eq(currentImageIndex).css('display', 'block')
+
+
+    });
 
 
 
