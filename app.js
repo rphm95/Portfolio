5daa9bea5b28f312031fd0a1e758afef41a1fa24
rafaela bootcamp <rafaelabootcamp@rafaelas-air.myfiosgateway.com>
2022-11-03 18:29:42 -0400

    main page

--- app.js
+++ app.js
@@ -67,9 +67,9 @@ $ (() => {
 
 
     // Set a same-site cookie for first-party contexts
-document.cookie = 'cookie1=value1; SameSite=Lax';
-// Set a cross-site cookie for third-party contexts
-// document.cookie = 'cookie2=value2; SameSite=None; Secure';
+    document.cookie = 'cookie1=value1; SameSite=Lax';
+    // Set a cross-site cookie for third-party contexts
+    // document.cookie = 'cookie2=value2; SameSite=None; Secure';
 
 })
 
